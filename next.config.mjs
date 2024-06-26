4f2ad4290954f571fa68bee245d64c25ace3341b
omar ashraf <omarashrafothman1234@gmail.com>
2024-06-26 14:02:26 +0300

    webback config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "postcss-loader",
+          options: {
+            postcssOptions: {
+              plugins: [["postcss-preset-env", {}]],
+            },
+          },
+        },
+        "resolve-url-loader",
+        "sass-loader",
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
